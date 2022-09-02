f773d35c89d5e08827df107ec75b0b6e1b564612
tanvirstreame <engr.tanvir.islam.1@gmail.com>
2022-09-03 00:37:04 +0600

    Added book class

---
+++ class/book.js
@@ -0,0 +1,10 @@
+var Book = /** @class */ (function () {
+    function Book() {
+    }
+    return Book;
+}());
+var book = new Book();
+book.id = 0;
+book.author = "Tanvir";
+book.price = 20;
+console.log(book);
---
+++ class/book.ts
@@ -0,0 +1,11 @@
+class Book {
+  id: number;
+  author: string;
+  price: number
+}
+ 
+const book = new Book();
+book.id = 0;
+book.author = "Tanvir";
+book.price = 20
+console.log(book);
